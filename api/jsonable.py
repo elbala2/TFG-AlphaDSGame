b7a8a092fdf0c72ec2eafd05f6b9021bad84615b
elbala2 <ealbala2@gmail.com>
2023-02-13 20:46:45 +0100

    wip

--- api/jsonable.py
+++ api/jsonable.py
@@ -2,11 +2,4 @@ import json
 
 class JSONObj:
   def toJSON(self):
-    return json.dumps(self, default=lambda o: o.__dict__)
-
-class ComplexEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Abc) or isinstance(obj, Doc):
-            return obj.toJSON()
-        else:
-            return json.JSONEncoder.default(self, obj)
+    return json.dumps(self, default=lambda o: getattr(o, 'toJSON', str(o)))
