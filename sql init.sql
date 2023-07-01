1ce28c6ed0d784350662cfe2539ae5963124c20b
elbala2 <ealbala2@gmail.com>
2023-07-01 17:36:50 +0200

    DataBase migration complete

--- sql init.sql
+++ sql init.sql
@@ -1,10 +1,14 @@
+drop database tfg;
+
+create database tfg;
+
 drop table tfg.games;
 
 create table tfg.Games (
   gameId int primary key not null AUTO_INCREMENT,
-  gameState blob,
-  PRIMARY KEY (gameId)
-)
-AUTO_INCREMENT = 1;
+  gameState blob
+);
+
+ALTER TABLE tfg.Games AUTO_INCREMENT = 1;
 
 select * from tfg.Games;
